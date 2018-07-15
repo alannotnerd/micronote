3bac126eb169d64d26f6a56ca768186adef771e8
Alan WANG <alan1995wang@outlook.com>
2018-07-16 07:05:03 +0800

    autojust preview height

--- app/assets/javascripts/project/edit.js
+++ app/assets/javascripts/project/edit.js
@@ -5,11 +5,12 @@ $(document).ready(function (){
 });
 
 
-function removeHeader(){
+function removeHeader(obj){
   $("#pif").contents().find("#header-container").hide();
   $("#pif").contents().find("#menubar").hide();
   // $("#pif").contents().find("#header").hide();
   $("#pif").show();
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
   // $(($("#pif").contents()[0])).ready(function(){
   //   setTimeout($("#pif").contents().find("#header").hide(),500);
   // })
