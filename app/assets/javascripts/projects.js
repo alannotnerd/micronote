e223b5bea0f2f521f982901d0d166ae91c550b4c
Alan WANG <alan1995wang@outlook.com>
2018-09-10 00:42:58 +0800

    full screen

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,4 @@
 function autoResize(obj){
-  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  var height = obj.contentWindow.document.body.scrollHeight;
+  obj.style.height = (height > 768)? height+'px':'768px'
 }
