db8661bc05f4ac6b60bdc078a4660bc32c4f5afa
Alan WANG <alan1995wang@outlook.com>
2018-09-09 23:18:03 +0800

    add slide show toolbar

--- app/assets/javascripts/project/edit.js
+++ app/assets/javascripts/project/edit.js
@@ -2,17 +2,58 @@ $(document).ready(function (){
   $("#pif").hide();
 });
 
+// function cellTypeHandle(obj){
+//   if(obj.value === "None"){
+//     $("li[data-name='None'] a").click();
+//   }else if(obj.value === "SlideShow"){
+//     $("li[data-name='Slideshow'] a").click();
+//   }
+// }
+
+function toggleFullScreen() {
+  $("#pif").addClass("full-screen");
+}
+
 
 function removeHeader(obj){
   _a = $("#pif").contents().find("#kernel_indicator")
   $("#pif").contents().find("#maintoolbar-container").append(_a)
   $("#pif").contents().find("#header-container").hide();
   $("#pif").contents().find("#menubar").hide();
+  // $("#pif").contents().find()\
+  $("#pif").contents().find("#maintoolbar-container").append(
+    `
+      <select id="show_type" class="form-control select-xs" onchange='$("li[data-name="+this.value+"] a").click()'>
+        <option value="None">None</option>
+        <option value="Slideshow">Slide Show</option>
+      </select>
+    `
+  );
+
+  $("#pif").contents().find("#maintoolbar-container").append(
+    `
+      <div class="btn-group">
+        <button class="btn btn-default" id="full_screen_btn">
+          <i class="fa-expand fa"></i>
+        </button>
+      </div>
+    `
+  );
   // $("#pif").contents().find("#header").hide();
-  $("#pif").show();
-  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-  // $(($("#pif").contents()[0])).ready(function(){
-  //   setTimeout($("#pif").contents().find("#header").hide(),500);
+  // $("#pif").contents().find("select#show_type").change(function (){
+  //   if(this.value === "None"){
+  //     // console.log("what the fuck None");
+  //     $("li[data-name='None'] a").click();
+  //   }else if(this.value === "SlideShow"){
+  //     // console.log("what the fuck Show");
+  //     $("li[data-name='Slideshow'] a").click();
+  //     // alert("clicked");
+  //   }
   // })
+  $("#pif").show();
+  $("#full_screen_btn").click(function (){
+    $("#pif").addClass("full-screen");
+  })
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
 
